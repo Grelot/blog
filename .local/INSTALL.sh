209c65fb461859c1d5241b797ef0e2e483ad162f
peguerin <pierre-edouard.guerin@florimond-desprez.fr>
2025-06-24 10:44:02 +0200

    instructions for local install

---
+++ .local/INSTALL.sh
@@ -0,0 +1,4 @@
+wget https://github.com/getzola/zola/releases/download/v0.20.0/zola-v0.20.0-x86_64-unknown-linux-gnu.tar.gz
+tar xvzf zola-v0.20.0-x86_64-unknown-linux-gnu.tar.gz
+sudo mv zola /usr/local/bin 
+mv zola-v0.20.0-x86_64-unknown-linux-gnu.tar.gz .local
